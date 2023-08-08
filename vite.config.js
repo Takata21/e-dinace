1409f816b6ae70df157f5f8d96c00d41de3e4136
Manuel Takata <takata211011@gmail.com>
2023-08-08 13:50:58 -0500

    remove million.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import million from 'million/compiler'
+// import million from 'million/compiler'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
+  // plugins: [million.vite({ auto: true }), react()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
