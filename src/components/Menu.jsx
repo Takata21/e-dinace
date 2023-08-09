4abbbf900191e5cf6aa487b6521592f0241d89f8
Manuel Takata <takata211011@gmail.com>
2023-08-08 22:21:56 -0500

    add menu component

---
+++ src/components/Menu.jsx
@@ -0,0 +1,52 @@
+import { Link } from 'react-router-dom'
+
+export const Menu = ({ Menuref }) => {
+  return (
+    <nav className="menu md:flex-[2] md:justify-end" ref={Menuref}>
+      <ul className="flex flex-col p-4 h-full text-center md:flex-row md:gap-5 md:justify-end ">
+        <li>
+          <a
+            className="block py-4 text-[#0c163b] font-medium text-xl md:py-0 links-transitions md:relative"
+            href="https://dinace.utp.ac.pa/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Inicio
+          </a>
+        </li>
+        <li>
+          <Link
+            href="#"
+            className="block py-4 text-[#0c163b] font-medium text-xl md:py-0 links-transitions md:relative"
+          >
+            Efemérides
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="#"
+            className="block py-4 text-[#0c163b] font-medium text-xl md:py-0 links-transitions md:relative"
+          >
+            Constelaciones
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="#"
+            className="block py-4 text-[#0c163b] font-medium text-xl md:py-0 links-transitions md:relative"
+          >
+            Fases De La Luna
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="#"
+            className="block py-4 text-[#0c163b] font-medium text-xl md:py-0 links-transitions md:relative"
+          >
+            Eclipses
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { useRef } from 'react'
+import { Menu } from '@/components/Menu.jsx'
+export const Navbar = () => {
+  const MenuBtn = useRef(null)
+  const handleClick = () => {
+    MenuBtn.current.classList.toggle('active')
+  }
+  return (
+    <header className="bg-white border-gray-200 dark:bg-gray-900 h-20 ">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto  relative md:flex-row md:flex-nowrap">
+        <div className="p-4 flex justify-between items-center w-full md:w-auto md:flex-1">
+          <a
+            href="https://dinace.utp.ac.pa/"
+            className="flex items-center"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="./images/dinace_logo.png"
+              className="h-16 mr-3"
+              alt="dinace Logo"
+            />
+            <div>
+              <span className="self-center  font-semibold text-base whitespace-nowrap dark:text-white">
+                Universidad Tecnológica de Panamá
+              </span>
+              <h2 className="text-base font-bold text-[#29166F]">
+                Dirección Nacional de Ciencias Espaciales
+              </h2>
+            </div>
+          </a>
+          <div className="relative w-10 md:hidden">
+            <button className="btn" onClick={() => handleClick()}>
+              <div className=""></div>
+            </button>
+          </div>
+        </div>
+        <Menu Menuref={MenuBtn} />
+      </div>
+    </header>
+  )
+}
