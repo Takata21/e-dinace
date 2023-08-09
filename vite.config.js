8f197047538817731d382204d4486222c2e9b099
Manuel Takata <takata211011@gmail.com>
2023-08-08 22:34:59 -0500

    remove alias import config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 // import million from 'million/compiler'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   // plugins: [million.vite({ auto: true }), react()],
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src/'),
-    },
-  },
+  resolve: {},
 })
