bdacdda15efcfc68fc1e44cd9a6bee9caf21b10a
Manuel Takata <takata211011@gmail.com>
2023-08-10 00:04:20 -0500

    add videos

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import { Slider } from '../components'
 
 export function HomePage() {
-  return <div className="min-h-screen">HomePage</div>
+  return (
+    <div className="relative min-h-screen">
+      {/* <h1 className="absolute z-10 w-full font-semibold text-center text-white uppercase font-Space">
+        Descubre Efemérides, Galaxias,
+        <br className="md:hidden" /> Eclipses y más
+      </h1> */}
+      <Slider />
+    </div>
+  )
 }
