4d2dd70213e68f8a823241573bf81eef91731206
Manuel Takata <takata211011@gmail.com>
2023-08-10 01:45:58 -0500

    add large device styles

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,60 +1,66 @@
 import { Carousel } from 'flowbite-react'
 import { VideoBackground } from './index'
-import { ArrowRight } from './Icons'
+import { FaArrowRightLong } from 'react-icons/fa6'
 import { Link } from 'react-router-dom'
 export const Slider = () => {
   return (
-    <div className="relative h-80">
-      <Carousel slideInterval={6000} className="relative h-80 carousel">
-        <div className="flex flex-col items-end justify-end h-full pl-5 text-left pb-9 dark:text-white">
+    <div className="relative h-80 md:min-h-[calc(100vh-72px)]">
+      <Carousel
+        slideInterval={6000}
+        className="relative h-80 carousel md:min-h-[calc(100vh-72px)]"
+      >
+        <div className="flex flex-col items-end justify-end h-full pl-5 text-left pb-9 lg:justify-center lg:items-start lg:gap-5">
           <VideoBackground source="/videos/milky_way_glowing_at_night.mp4" />
-          <h3 className="text-[#da374f] font-Space text-xl font-bold block uppercase text-left w-full tracking-[3px]">
+          <h3 className="text-[#da374f] font-Space text-xl font-bold block uppercase text-left w-full tracking-[3px] lg:text-3xl">
             {/* DESCUBRE CONFINES ESTELARES. */}
             Efemérides
           </h3>
           <Link
             to="/"
-            className="text-2xl font-semibold text-white text-gradient"
+            className="w-full text-2xl font-semibold text-white text-gradient lg:text-5xl lg:w-[620px]"
           >
             Explorando el Calendario Cósmico, Tras los Pasos de los Astros
-            <ArrowRight />
+            <FaArrowRightLong className="arrow lg:my-5" color="#da374f" />
           </Link>
         </div>
-        <div className="flex flex-col items-end justify-end h-full pl-5 text-left pb-9 dark:text-white ">
+        <div className="flex flex-col items-end justify-end h-full pl-5 text-left pb-9 lg:justify-center lg:items-start lg:gap-5">
           <VideoBackground source="/videos/constellations.mp4" />
-          <h3 className="text-[#da374f] font-Space text-xl font-bold block uppercase text-left w-full tracking-[3px]">
+          <h3 className="text-[#da374f] font-Space text-xl font-bold block uppercase text-left w-full tracking-[3px] lg:text-3xl">
             Constelaciones
           </h3>
-          <Link className="w-full text-2xl font-semibold text-left text-white">
-            Las Constelaciones nos Guían <ArrowRight />
+          <Link className="w-full text-2xl font-semibold text-white text-gradient lg:text-5xl lg:w-[620px]">
+            Las Constelaciones nos Guían{' '}
+            <FaArrowRightLong className="arrow lg:my-5" color="#da374f" />
           </Link>
         </div>
-        <div className="flex flex-col items-end justify-end h-full pl-5 text-left pb-9 dark:text-white ">
+        <div className="flex flex-col items-end justify-end h-full pl-5 text-left pb-9 lg:justify-center lg:items-start lg:gap-5">
           <VideoBackground source="/videos/galaxy.mp4" />
-          <h3 className="text-[#da374f] font-Space text-xl font-bold block uppercase text-left w-full tracking-[3px]">
+          <h3 className="text-[#da374f] font-Space text-xl font-bold block uppercase text-left w-full tracking-[3px] lg:text-3xl">
             Galaxias
           </h3>
-          <Link className="w-full text-2xl font-semibold text-left text-white">
+          <Link className="w-full text-2xl font-semibold text-white text-gradient lg:text-5xl lg:w-[620px]">
             NAVEGA ENTRE LAS CONSTELACIONES DE GALAXIAS
-            <ArrowRight />
+            <FaArrowRightLong className="arrow lg:my-5" color="#da374f" />
           </Link>
         </div>
-        <div className="flex flex-col items-end justify-end h-full pl-5 text-left pb-9 dark:text-white ">
+        <div className="flex flex-col items-end justify-end h-full pl-5 text-left pb-9 lg:justify-center lg:items-start lg:gap-5">
           <VideoBackground source="/videos/eclipse.mp4" />
-          <h3 className="text-[#da374f] font-Space text-xl font-bold block uppercase text-left w-full tracking-[3px]">
+          <h3 className="text-[#da374f] font-Space text-xl font-bold block uppercase text-left w-full tracking-[3px] lg:text-3xl">
             Eclipses
           </h3>
-          <Link className="w-full text-2xl font-semibold text-left text-white">
-            ENTRE LUZ Y SOMBRA <ArrowRight />
+          <Link className="w-full text-2xl font-semibold text-white text-gradient lg:text-5xl lg:w-[620px]">
+            ENTRE LUZ Y SOMBRA{' '}
+            <FaArrowRightLong className="arrow lg:my-5" color="#da374f" />
           </Link>
         </div>
-        <div className="flex flex-col items-end justify-end h-full pl-5 text-left pb-9 dark:text-white ">
+        <div className="flex flex-col items-end justify-end h-full pl-5 text-left pb-9 lg:justify-center lg:items-start lg:gap-5">
           <VideoBackground source="/videos/MoonPhase.mp4" />
-          <h3 className="text-[#da374f] font-Space text-xl font-bold block uppercase text-left w-full tracking-[3px]">
+          <h3 className="text-[#da374f] font-Space text-xl font-bold block uppercase text-left w-full tracking-[3px] lg:text-3xl">
             Fases de la Luna
           </h3>
-          <Link className="w-full text-2xl font-semibold text-left text-white">
-            EXPLORA LAS FASES OCULTAS DE LA LUNA <ArrowRight />
+          <Link className="w-full text-2xl font-semibold text-white text-gradient lg:text-5xl lg:w-[620px]">
+            EXPLORA LAS FASES OCULTAS DE LA LUNA{' '}
+            <FaArrowRightLong className="arrow lg:my-5" color="#da374f" />
           </Link>
         </div>
       </Carousel>
