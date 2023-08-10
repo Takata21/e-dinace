8dbb1ac9684fc9b34be05fa4d4b2d646cbab4a47
Manuel Takata <takata211011@gmail.com>
2023-08-10 01:45:28 -0500

    remove icons

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,22 +1,2 @@
 export { MdMenu as MenuIcon } from 'react-icons/md'
 export { MdClose as CloseIcon } from 'react-icons/md'
-export { MdArrowRightAlt } from 'react-icons/md'
-export const ArrowRight = () => {
-  return (
-    <svg
-      width="56"
-      height="16"
-      viewBox="0 0 56 16"
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden="true"
-      focusable="false"
-      className="text-[#da374f]"
-    >
-      <path
-        d="M17.935.929L25.006 8l-7.071 7.071-1.414-1.414L21.177 9H1V7h20.177l-4.656-4.657L17.935.93z"
-        fill="currentColor"
-        fillRule="evenodd"
-      ></path>
-    </svg>
-  )
-}
