84780779f2701109d9d36d11082ab206b0f2c804
Manuel Takata <takata211011@gmail.com>
2023-08-11 02:18:28 -0500

    add constellation page

---
+++ src/pages/Constellation.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function Constellation() {
+  return <div>Constellation</div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,3 @@
 export { HomePage } from './HomePage'
+export { Constellations } from './Constellations'
+export { Constellation } from './Constellation'
