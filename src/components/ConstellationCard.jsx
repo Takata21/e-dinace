76589705e94d8497acea2f356ef46de370617b17
Manuel Takata <takata211011@gmail.com>
2023-08-13 00:48:06 -0500

    fix constellation link and styles

--- src/components/ConstellationCard.jsx
+++ src/components/ConstellationCard.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 export function ConstellationCard({ constellation }) {
   return (
     <Link
-      to={`./${constellation.replace(/\s+/g, '-')}`}
+      to={`./${constellation.toLowerCase().replace(/\s+/g, '-')}`}
       className="h-56 group lg:flex-grow"
     >
-      <div className="relative h-full font-semibold tracking-wider text-white font-Space">
+      <div className="relative h-full font-semibold tracking-wider text-white uppercase font-Bellefair">
         <img
           src={`/images/constellations/${constellation}.webp`}
           alt={`${constellation} constellation`}
@@ -14,7 +14,7 @@ export function ConstellationCard({ constellation }) {
           className="w-full h-full"
         />
         <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center m-auto text-xl transition duration-500 ease-in group-hover:bg-black/50">
-          <h5 className="transition duration-500 ease-in group-hover:scale-150">
+          <h5 className="transition duration-500 ease-in group-hover:scale-150 font-Bellefair">
             {constellation}
           </h5>
         </div>
