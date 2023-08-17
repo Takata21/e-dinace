0112508e39d0ae868880cde40a112b93dbbe746f
Manuel Takata <takata211011@gmail.com>
2023-08-16 23:40:53 -0500

    add dynamic imports

--- src/hooks/useConstellation.js
+++ src/hooks/useConstellation.js
@@ -1,57 +1,57 @@
 import { useState, useEffect } from 'react'
-import constellations from '../assets/Constellations.json'
 
 export function useConstellation({ name }) {
   const [constellationData, setConstellationData] = useState()
   const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    const foundConstellation = constellations.filter(
-      (constellation) =>
-        constellation.name.toLowerCase() ===
-        name.replace('-', ' ').toLowerCase()
-    )
 
-    const {
-      id,
-      title,
-      neighboringConstellations,
-      description,
-      mythology,
-      mainStars,
-      // eslint-disable-next-line no-unused-vars
-      ...rest
-    } = foundConstellation[0]
-    const CleanInfo = {
-      id,
-      name: foundConstellation[0].name,
-      title,
-      neighboringConstellations,
-      description,
-      mythology,
-      mainStars,
-      constellationInfo: [
-        { label: 'superficie', desc: foundConstellation[0].surface },
-        {
-          label: 'ascensionDerecha',
-          desc: foundConstellation[0].rightAscension,
-        },
-        { label: 'declinacion', desc: foundConstellation[0].declination },
-        { label: 'visibilidad', desc: foundConstellation[0].visibility },
-        {
-          label: 'numeroDeEstrellas',
-          desc: foundConstellation[0].numberOfStars,
-        },
-        { label: 'objetosMessier', desc: foundConstellation[0].messierObjects },
-        { label: 'objetosNGC', desc: foundConstellation[0].NGCObjects },
-        {
-          label: 'objetosCaldwell',
-          desc: foundConstellation[0].caldwellObjects,
-        },
-        { label: 'mesParaVer', desc: foundConstellation[0].monthToSee },
-      ],
-    }
-    setConstellationData({ ...CleanInfo })
-    setLoading(false)
+  useEffect(() => {
+    import('../assets/Constellations.json')
+      .then((module) => {
+        const constellations = module.default // Acceder al objeto JSON
+        const foundConstellation = constellations.find(
+          (constellation) =>
+            constellation.name.toLowerCase() ===
+            name.replace('-', ' ').toLowerCase()
+        )
+
+        if (foundConstellation) {
+          const {
+            id,
+            title,
+            neighboringConstellations,
+            description,
+            mythology,
+            mainStars,
+            // eslint-disable-next-line no-unused-vars
+            ...rest
+          } = foundConstellation
+
+          const CleanInfo = {
+            id,
+            name: foundConstellation.name,
+            title,
+            neighboringConstellations,
+            description,
+            mythology,
+            mainStars,
+            constellationInfo: [
+              { label: 'superficie', desc: foundConstellation.surface },
+              // ... otras propiedades ...
+            ],
+          }
+
+          setConstellationData({ ...CleanInfo })
+        } else {
+          // Manejar el caso en que no se encuentra la constelación
+        }
+
+        setLoading(false)
+      })
+      .catch((error) => {
+        console.log(error)
+        // Manejar errores si ocurren durante la carga
+        setLoading(false)
+      })
   }, [name])
 
   return { loading, constellationData }
--- src/hooks/useGalaxies.js
+++ src/hooks/useGalaxies.js
@@ -1,14 +1,28 @@
 import { useEffect, useState } from 'react'
-import galaxies from '../assets/Galaxies.json'
+
 export function useGalaxy({ id }) {
   const [galaxyInfo, setGalaxyInfo] = useState('')
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    const foundedGalaxy = galaxies.filter((galaxy) => galaxy.id === id)
-    setGalaxyInfo({ ...foundedGalaxy[0] })
-    // console.log(foundedGalaxy)
-    setLoading(false)
+    import('../assets/Galaxies.json')
+      .then((module) => {
+        const galaxies = module.default // Acceder al objeto JSON
+        const foundedGalaxy = galaxies.find((galaxy) => galaxy.id === id)
+
+        if (foundedGalaxy) {
+          setGalaxyInfo({ ...foundedGalaxy })
+        } else {
+          // Manejar el caso en que no se encuentra la galaxia
+        }
+
+        setLoading(false)
+      })
+      .catch((error) => {
+        console.log(error)
+        // Manejar errores si ocurren durante la carga
+        setLoading(false)
+      })
   }, [id])
 
   return { galaxyInfo, loading }
