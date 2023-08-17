c6c3953f676fc7475eab15713802ccb0e50628fe
Manuel Takata <takata211011@gmail.com>
2023-08-16 23:42:24 -0500

    add filter components for information

---
+++ src/components/SelectConstellations.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export function SelectConstellations() {
+  return (
+    <div className="lg:w-64">
+      <label
+        htmlFor="small"
+        className="block mt-5 mb-3 text-sm font-medium text-gray-900 dark:text-white"
+      >
+        Filtrar
+      </label>
+      <select
+        id="small"
+        className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg lg:mb-0 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      >
+        <option defaultValue>Todas</option>
+        <option value="1">Nombre</option>
+        <option value="2">Objetos Messier</option>
+        <option value="3">Número de Estrellas</option>
+        <option value="4">Objetos NGC</option>
+        <option value="5">Objetos Caldwell</option>
+      </select>
+    </div>
+  )
+}
