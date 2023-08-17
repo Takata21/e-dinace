f74f264c072b6f3d9b48334df711995ed4a68d45
Manuel Takata <takata211011@gmail.com>
2023-08-16 23:37:16 -0500

    add dark mode support

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,14 +1,15 @@
-import { Header, Footer } from './components/index'
+import { Navbar, Footer } from './components/index'
 import { Outlet } from 'react-router-dom'
+import { Flowbite } from 'flowbite-react'
 
 export const Layout = () => {
   return (
-    <>
-      <Header />
-      <main className="min-h-screen">
+    <Flowbite>
+      <Navbar />
+      <main className="min-h-screen dark:bg-[#18202b] dark:text-white bg-white">
         <Outlet />
       </main>
       <Footer />
-    </>
+    </Flowbite>
   )
 }
