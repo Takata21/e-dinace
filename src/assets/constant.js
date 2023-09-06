5ccd09853ce4551fe7e5b0ccd3970447d12afae0
Manuel Takata <takata211011@gmail.com>
2023-09-05 22:39:41 -0500

    add constants

---
+++ src/assets/constant.js
@@ -0,0 +1,13 @@
+// Tipos generales de eclipses
+export const SOLAR_ECLIPSE = 'Solar Eclipses'
+export const LUNAR_ECLIPSE = 'Lunar Eclipses'
+
+// Solar eclipse types
+export const SOLAR_ECLIPSE_TOTAL = 'Total Solar Eclipse'
+export const SOLAR_ECLIPSE_PARTIAL = 'Partial Solar Eclipse'
+export const SOLAR_ECLIPSE_ANNULAR = 'Annular Solar Eclipse'
+
+// Lunar eclipse types
+export const LUNAR_ECLIPSE_TOTAL = 'Total Lunar Eclipse'
+export const LUNAR_ECLIPSE_PARTIAL = 'Partial Lunar Eclipse'
+export const LUNAR_ECLIPSE_PENUMBRAL = 'Penumbral Lunar Eclipse'
