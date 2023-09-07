68c2d1796579fe6f5c452e28a587a2bacd1b3343
Manuel Takata <takata211011@gmail.com>
2023-09-07 00:36:07 -0500

    add eclipses types

--- src/assets/constant.js
+++ src/assets/constant.js
@@ -11,3 +11,24 @@ export const SOLAR_ECLIPSE_ANNULAR = 'Annular Solar Eclipse'
 export const LUNAR_ECLIPSE_TOTAL = 'Total Lunar Eclipse'
 export const LUNAR_ECLIPSE_PARTIAL = 'Partial Lunar Eclipse'
 export const LUNAR_ECLIPSE_PENUMBRAL = 'Penumbral Lunar Eclipse'
+
+export const LUNAR_ECLIPSE_TYPES = {
+  Nb: 'Eclipse Penumbral Total',
+  N: 'Eclipse Penumbral',
+  P: 'Eclipse Parcial',
+  T: 'Eclipse Total',
+  'T+': 'Eclipse Total +',
+  'T-': 'Eclipse Total -',
+  Nx: 'Eclipse Penumbral Extra',
+}
+
+export const SOLAR_ECLIPSE_TYPES = {
+  Pb: 'Eclipse  Parcial con Besseliano',
+  P: 'Eclipse  Parcial',
+  Ts: 'Eclipse  Total con Sombra de la Tierra',
+  Am: 'Eclipse  Anular con Magnitud Máxima',
+  A: 'Eclipse  Anular',
+  Pe: 'Eclipse  Parcial Extra',
+  T: 'Eclipse Total',
+  H: 'Eclipse Híbrido',
+}
