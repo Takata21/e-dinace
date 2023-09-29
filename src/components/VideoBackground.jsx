bd418e6168275b7cd3c2bdd98b4a05205ff88128
Manuel Takata <takata211011@gmail.com>
2023-09-29 15:03:25 -0500

    active loop videos

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -4,7 +4,7 @@ export const VideoBackground = ({ source }) => {
   return (
     <video
       autoPlay
-      // loop
+      loop
       muted
       className="absolute top-0 bottom-0 left-0 right-0  object-cover w-full h-full gradient-carousel -z-[1]"
     >
