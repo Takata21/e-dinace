07ce1bcee951bcc2d69d1eb9fb1ab00dcc7a7c83
Manuel Takata <takata211011@gmail.com>
2023-09-29 22:51:05 -0500

    Implemented theme color auto-detection

--- src/context/provider/ThemeContext.jsx
+++ src/context/provider/ThemeContext.jsx
@@ -1,13 +1,23 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 
 const ThemeContext = createContext()
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light') // Puedes comenzar con el tema "light" por defecto
+  // Comenzamos con el tema "light" por defecto
+  const [theme, setTheme] = useState('light')
 
+  // Función para cambiar el tema
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'))
   }
+
+  // Detectar el modo preferido del sistema y establecer el tema en consecuencia
+  useEffect(() => {
+    const prefersDarkMode = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    ).matches
+    setTheme(prefersDarkMode ? 'dark' : 'light')
+  }, [])
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
