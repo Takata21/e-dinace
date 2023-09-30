0b4a9d52f6ebe3129daaf0b3af9f4da25f2563f7
Manuel Takata <takata211011@gmail.com>
2023-09-29 22:53:08 -0500

    Fix menu button not displaying on medium screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ export const Navbar = () => {
           <div className="xl:mr-4">
             <DarkThemeToggle />
             <button
-              className="p-2 text-gray-500 rounded cursor-pointer xl:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              className="p-2 text-gray-500 rounded cursor-pointer 1xl:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
               onClick={() => handleClick()}
             >
               {menu ? <CgClose size={24} /> : <CgMenuRightAlt size={24} />}
