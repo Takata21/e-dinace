eed8d8e13be17ef5dd04be6bb80a7ab99c68ff8b
Manuel Takata <takata211011@gmail.com>
2023-10-05 22:32:45 -0500

    add constellation test

--- src/pages/Constellation.jsx
+++ src/pages/Constellation.jsx
@@ -21,18 +21,25 @@ export function Constellation() {
                 src={`/images/constellations/${constellationData?.name}.webp`}
                 alt=""
                 className="xl:min-w-[350px] xl:min-h-[300px]"
+                data-testid="constellation-img"
               />
             </Zoom>
             <div>
               <h2 className="mb-5 text-4xl font-bold text-center uppercase font-Antonio xl:text-left xl:text-7xl ">
                 {constellationData?.name}
               </h2>
-              <p className="font-Metropolis dark:text-white xl:leading-7">
+              <p
+                className="font-Metropolis dark:text-white xl:leading-7"
+                data-testid="constellation-desc"
+              >
                 {constellationData?.description}
               </p>
             </div>
           </div>
-          <ul className="flex flex-col flex-wrap gap-3 my-5 xl:flex-row">
+          <ul
+            className="flex flex-col flex-wrap gap-3 my-5 xl:flex-row"
+            data-testid="constellation-info-list"
+          >
             {constellationData?.constellationInfo?.map((info, index) => {
               return (
                 <InfoCard key={index} label={info.label} desc={info.desc} />
